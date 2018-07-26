9cd037ed306435877ce0f56bfb38646a5399fc9e
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2018-07-26 13:13:50 -0300

    Commit inicial

---
+++ src/main/java/br/org/otojunior/sample/springbootbatch/SpringbootBatchApplication.java
@@ -0,0 +1,12 @@
+package br.org.otojunior.sample.springbootbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBatchApplication.class, args);
+	}
+}
